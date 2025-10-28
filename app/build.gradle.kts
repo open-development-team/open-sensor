0b2faeb4aa5b3313f4cc7f8f25d00f2dbd7207aa
Tolga Hoşgör <fasdfasdas@gmail.com>
2025-10-28 16:05:51 +0300

    fix: automatically start enabled service after an app update
    Signed-off-by: Tolga Hoşgör <fasdfasdas@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
         applicationId = "com.opendevelopment.opensensor"
         minSdk = 24
         targetSdk = 36
-        versionCode = 1
+        versionCode = 2
         versionName = "0.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
--- app/src/main/java/com/opendevelopment/opensensor/BootReceiver.kt
+++ app/src/main/java/com/opendevelopment/opensensor/BootReceiver.kt
@@ -8,7 +8,7 @@ import kotlinx.coroutines.runBlocking
 
 class BootReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent) {
-        if (intent.action == Intent.ACTION_BOOT_COMPLETED) {
+        if (intent.action == Intent.ACTION_BOOT_COMPLETED || intent.action == Intent.ACTION_MY_PACKAGE_REPLACED) {
             runBlocking {
                 val settingsDataStore = SettingsDataStore(context)
                 val settings = settingsDataStore.settingsFlow.first()
